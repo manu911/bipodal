1a0c2632bd9e698e462a55ddb5d4ab2b5224c1c0
manu911 <manugc911@gmail.com>
2025-01-14 21:01:57 +0100

    Update page.tsx

--- bipodal/src/app/page.tsx
+++ bipodal/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           width={180}
           height={38}
           priority
-        />
+        />BIPODAL
         <ol>
           <li>
             Get started by editing <code>src/app/page.tsx</code>.
