b70146d1158d697d45d881240d9ab362261ef8fa
manu911 <manugc911@gmail.com>
2025-01-25 12:01:12 +0100

    config env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: "/page",
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
+
