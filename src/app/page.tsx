16a697c3dd738b3feb2a35a09e5c6bcc3bee191e
manu911 <manugc911@gmail.com>
2025-01-25 20:38:26 +0100

    adding html and body to root component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 
 import Layout from "@/common/layout/layout";
+import Head from 'next/head';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-export default function Home() {
+export default function Page() {
   return (
-    <Layout>asdas</Layout>
+    <html>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <body>
+        <Layout>asdas</Layout>
+      </body>
+    </html>
   );
 }
--- src/common/layout/layout.tsx
+++ src/common/layout/layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import Header from '../header/header'
 import Footer from '../footer/footer'
-import Head from 'next/head';
 import styles from './layout.module.css'
 
 interface HeaderProps {
@@ -9,11 +8,7 @@ interface HeaderProps {
 }
 export default function Layout({children}:HeaderProps) {
   return (
-    <html>
-    <Head>
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    </Head>
-    <body>
+   <>
       <div className={styles.page}>
         <Header></Header>
         <main className={styles.main}>
@@ -21,7 +16,7 @@ export default function Layout({children}:HeaderProps) {
         </main>
       </div>
       <Footer></Footer>
-    </body>
-  </html>
+      </>
+ 
   )
 }
