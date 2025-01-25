4ff9fbb6ea5a9655736c0396d13671ef6b380309
manu911 <manugc911@gmail.com>
2025-01-25 12:09:41 +0100

    ASDAS

--- src/app/QuienesSomos/page.tsx
+++ src/app/QuienesSomos/page.tsx
@@ -10,7 +10,7 @@ export default function Servicios() {
   return (
     <Layout>
       <div className={styles.page}>
-        El Garci & company
+        El Garci & companyasdfasdfa
       </div>
     </Layout>
   );
