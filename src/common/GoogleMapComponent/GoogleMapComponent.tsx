70ce0e104d4f45c2fb65fd3edbdd114b68cc8372
manu911 <manugc911@gmail.com>
2025-01-26 20:25:18 +0100

    infoview

--- src/common/GoogleMapComponent/GoogleMapComponent.tsx
+++ src/common/GoogleMapComponent/GoogleMapComponent.tsx
@@ -1,39 +1,45 @@
 'use client'
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, LoadScript, Marker } from '@react-google-maps/api';
 import { config } from 'dotenv';
 
 config();
-const mapskey :string = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY?.toString() || "";
+const mapskey: string = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY?.toString() || "";
 
 
 const center = {
-  lat: 38.363133961428275, 
+  lat: 38.363133961428275,
   lng: -0.4935381577044199
 };
 
 const GoogleMapComponent = () => {
 
-const defaultMapContainerStyle = {
-  width: '400px',
-  height: '400px',
-  borderRadius: '20px',
-};
-const defaultMapOptions = {
-  zoomControl: true,
-  tilt: 0,
-  gestureHandling: 'auto',
-  mapTypeId: 'satellite',
-};
+  const defaultMapContainerStyle = {
+    width: '400px',
+    height: '400px',
+    borderRadius: '20px',
+  };
+  const defaultMapOptions = {
+    zoomControl: true,
+    tilt: 0,
+    gestureHandling: 'auto',
+    mapTypeId: 'satellite',
+  };
   return (
     <LoadScript {...{ "googleMapsApiKey": mapskey }}>
       <GoogleMap
         mapContainerStyle={defaultMapContainerStyle}
         center={center}
-        zoom={10}
+        zoom={20}
         options={defaultMapOptions}
       >
-        <Marker position={center} />
+        <InfoWindow
+          options={{ maxWidth: 300 }}>
+
+          <span>bipodalll</span>
+          <Marker position={center} />
+        </InfoWindow>
+
       </GoogleMap>
     </LoadScript>
   );
