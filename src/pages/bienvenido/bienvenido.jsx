fdfecf647eae155ff2db4043d807b09f8cf008c3
manu911 <manugc911@gmail.com>
2025-02-02 12:01:35 +0100

    text styles for bienvenido page

--- src/pages/bienvenido/bienvenido.jsx
+++ src/pages/bienvenido/bienvenido.jsx
@@ -9,7 +9,10 @@ export default function Bienvenido() {
         <div className={styles.containerPageBienvenido}>
             <div className={styles.textContainer}>
                 <h1>Bienvenido a nuestra clínica podológica</h1>
-                <p>En nuestro rincón dedicado al bienestar podológico, nos enorgullece ser tus guías en el camino hacia unos pies más saludables y una vida sin molestias. En Clínica de Podología Bipodal, liderada por el apasionado podólogo Alejandro García, entendemos que tus pies no solo te llevan a lugares, sino que también merecen una atención especializada. Aquí, en nuestra clínica, no solo tratamos los problemas podológicos, sino que también creamos una experiencia centrada en ti. Nos apasiona aliviar el dolor, mejorar la movilidad y asegurarnos de que cada paso que des sea más cómodo y saludable. A continuación, destacamos algunas de las ventajas que hacen que nuestra clínica sea única y especial: </p>
+                <p>En nuestro rincón dedicado al bienestar podológico, nos enorgullece ser tus guías en el camino hacia unos pies más saludables y una vida sin molestias. </p>
+                <p>En Clínica de Podología <b>Bipodal</b>, liderada por el apasionado podólogo <b>Alejandro García</b>, entendemos que tus pies no solo te llevan a lugares, sino que también merecen una atención especializada. 
+                </p>
+                <p>Aquí, en nuestra clínica, no solo tratamos los problemas podológicos, sino que también creamos una experiencia centrada en ti. Nos apasiona aliviar el dolor, mejorar la movilidad y asegurarnos de que cada paso que des sea más cómodo y saludable. A continuación, destacamos algunas de las ventajas que hacen que nuestra clínica sea única y especial: </p>
             </div>
 
             <div className={styles.clinicaImgContainer}>
