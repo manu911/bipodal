75cf9d2436deebbcb4e2e370e4e08ab87308545e
manu911 <manugc911@gmail.com>
2025-02-12 16:31:23 +0100

    url fixed for navbar

--- src/common/NavBar/NavBar.tsx
+++ src/common/NavBar/NavBar.tsx
@@ -11,16 +11,21 @@ import {
 } from 'material-ui-popup-state/hooks'
 import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import nextConfig from 'next.config';
 
 export default function NavBar() {
   const popupState = usePopupState({ variant: 'popover', popupId: 'demoMenu' })
+
+  function createPath(subroute: string) : string {
+    return nextConfig.basePath + subroute;
+  }
   return (
     <div className={styles.navbarContainer}>
       <div className={styles.navBarGroup} aria-label="Menu de navegacion">
-        <Link className={styles.link} href="/" underline="none" sx={{ textTransform: 'none' }}>Bienvenido/a</Link>
-        <Link className={styles.link} href="/Servicios" underline="none" sx={{ textTransform: 'none' }}>Servicios</Link>
-        <Link className={styles.link} href="/QuienesSomos" underline="none" sx={{ textTransform: 'none' }}>¿Quienes somos?</Link>
-        <Link className={styles.link} href="/Contacto" underline="none" sx={{ textTransform: 'none' }}>Contacto</Link>
+        <Link className={styles.link} href={createPath("/")} underline="none" sx={{ textTransform: 'none' }}>Bienvenido/a</Link>
+        <Link className={styles.link} href={createPath("/Servicios")} underline="none" sx={{ textTransform: 'none' }}>Servicios</Link>
+        <Link className={styles.link} href={createPath("/QuienesSomos")} underline="none" sx={{ textTransform: 'none' }}>¿Quienes somos?</Link>
+        <Link className={styles.link} href={createPath("/Contacto")} underline="none" sx={{ textTransform: 'none' }}>Contacto</Link>
 
       </div>
       <div className={styles.burguerNavBarGroup}>
@@ -32,10 +37,10 @@ export default function NavBar() {
             transformOrigin={{ horizontal: 'right', vertical: 'top' }}
             anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
             >
-          <MenuItem onClick={popupState.close}><Link className={styles.link} href="/" underline="none" sx={{ textTransform: 'none' }}>Bienvenido/a</Link></MenuItem>
-          <MenuItem  onClick={popupState.close}><Link className={styles.link} href="/Servicios" underline="none" sx={{ textTransform: 'none' }}>Servicios</Link></MenuItem>
-          <MenuItem onClick={popupState.close}> <Link className={styles.link} href="/QuienesSomos" underline="none" sx={{ textTransform: 'none' }}>¿Quienes somos?</Link></MenuItem>
-          <MenuItem onClick={popupState.close}> <Link className={styles.link} href="/Contacto" underline="none" sx={{ textTransform: 'none' }}>Contacto</Link>          </MenuItem>
+          <MenuItem onClick={popupState.close}><Link className={styles.link} href={createPath("/")} underline="none" sx={{ textTransform: 'none' }}>Bienvenido/a</Link></MenuItem>
+          <MenuItem  onClick={popupState.close}><Link className={styles.link} href={createPath("/Servicios")} underline="none" sx={{ textTransform: 'none' }}>Servicios</Link></MenuItem>
+          <MenuItem onClick={popupState.close}> <Link className={styles.link} href={createPath("/QuienesSomos")} underline="none" sx={{ textTransform: 'none' }}>¿Quienes somos?</Link></MenuItem>
+          <MenuItem onClick={popupState.close}> <Link className={styles.link} href={createPath("/Contacto")} underline="none" sx={{ textTransform: 'none' }}>Contacto</Link>          </MenuItem>
         </Menu>
       </div>
     </div>
