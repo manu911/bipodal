99ac7eeca4a21ee3c71c4505e470f6639f60f4b0
manu911 <manugc911@gmail.com>
2025-02-13 17:16:16 +0100

    colors

--- src/app/Servicios/page.tsx
+++ src/app/Servicios/page.tsx
@@ -2,7 +2,68 @@
 import Ribbon from '@/common/ribbon/ribbon';
 import styles from './Servicios.module.css';
 import Layout from "@/common/mainLayout/mainLayout";
+
+interface RibbonProps {
+  title: string;
+  buttonText: string;
+  imgSrc: string;
+  imgAlt: string;
+  children: string;
+  variant?: 'primary' | 'secondary' | 'tertiary'| 'quaternary';
+  imgLeft?: boolean;
+}
 export default function Servicios() {
+  
+  const data : Array<RibbonProps>=[
+    {
+    title: 'Ofrecemos nuestros servicios de podología a domicilio',
+    imgSrc: './papilomaPlantar.jpg',
+    imgAlt: 'pies',
+    buttonText: 'Pide tu cita a domicilio',
+    children: `En Clínica Bipodal, llevamos la excelencia de nuestros servicios directamente a tu hogar. Entendemos que la comodidad es clave cuando se trata de cuidar de tu salud podológica, por lo que ofrecemos nuestros servicios de podología a domicilio. Ya sea que te encuentres con limitaciones de movilidad, prefieras la privacidad de tu espacio o simplemente busques la máxima conveniencia, nuestro equipo de podólogos expertos está listo para brindarte el mismo nivel de atención especializada que encontrarías en nuestra clínica. Agenda tu cita y permite que el cuidado podológico se adapte a tu estilo de vida, llevando la calidad de nuestros servicios directamente a tu puerta.`,
+    variant: 'primary',
+    imgLeft: false
+  },
+  {
+    title: 'Papiloma plantar',
+    imgSrc: './papilomaPlantar.jpg',
+    imgAlt: 'pies',
+    buttonText: 'Más Información',
+    children: `La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.`,
+    variant: 'secondary',
+    imgLeft: true
+  },
+  {
+    title: 'Pedidos a domicilio',
+    imgSrc: '',
+    imgAlt: '',
+    buttonText: '',
+    children: `La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.`,
+    variant: 'quaternary',
+    imgLeft: true
+  }, 
+  {
+    title: 'Papiloma plantar',
+    imgSrc: './papilomaPlantar.jpg',
+    imgAlt: 'pies',
+    buttonText: 'Más Información',
+    children: `La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.`,
+    variant: 'secondary',
+    imgLeft: true
+  },
+  {
+    title: 'Pedidos a domicilio',
+    imgSrc: '',
+    imgAlt: '',
+    buttonText: '',
+    children: `La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.`,
+    variant: 'tertiary',
+    imgLeft: true
+  }, 
+
+]
+
+
   return (
     <Layout>
       <div className={styles.containerPageServicios}>
@@ -10,32 +71,12 @@ export default function Servicios() {
           <h2 className={styles.title}>Servicios de Podología en Alicante</h2>
           <p className={styles.description}>Bienvenido a nuestra página de servicios de podología en Alicante, donde tu comodidad y salud son nuestra prioridad. En Clínica Bipodal, nos enorgullece ofrecer una variedad de servicios podológicos especializados, diseñados para abordar tus necesidades individuales. Nuestro equipo de profesionales apasionados está dedicado a proporcionar soluciones efectivas y personalizadas para garantizar que cada paso que des sea cómodo y saludable.</p>
         </div>
-
-        <Ribbon variant='primary' imgLeft={false} buttonText='Pide tu cita a domicilio' imgAlt='pies' imgSrc='./papilomaPlantar.jpg' title='Ofrecemos nuestros servicios de podología a domicilio'>
-          En Clínica Bipodal, llevamos la excelencia de nuestros servicios directamente a tu hogar. Entendemos que la comodidad es clave cuando se trata de cuidar de tu salud podológica, por lo que ofrecemos nuestros servicios de podología a domicilio. Ya sea que te encuentres con limitaciones de movilidad, prefieras la privacidad de tu espacio o simplemente busques la máxima conveniencia, nuestro equipo de podólogos expertos está listo para brindarte el mismo nivel de atención especializada que encontrarías en nuestra clínica. Agenda tu cita y permite que el cuidado podológico se adapte a tu estilo de vida, llevando la calidad de nuestros servicios directamente a tu puerta.
-        </Ribbon>
-
-        <Ribbon variant='secondary' imgLeft={true} buttonText='Más Información' imgAlt='pies' imgSrc='./papilomaPlantar.jpg' title='Papiloma plantar'>
-          La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.
-        </Ribbon>
-
-        <Ribbon buttonText='Más Información' imgAlt='pies' imgSrc='./papilomaPlantar.jpg' title='Papiloma plantar'>
-          La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.
-        </Ribbon>
-
-        <Ribbon buttonText='Más Información' imgAlt='pies' imgSrc='./papilomaPlantar.jpg' title='Papiloma plantar'>
-          La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.
-        </Ribbon>
-
-        <Ribbon buttonText='Más Información' imgAlt='pies' imgSrc='./papilomaPlantar.jpg' title='Papiloma plantar'>
-          La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.
-        </Ribbon>
-
-        <h2></h2>
-        <p></p>
-        <button></button>
-
-        <h2>Descubre Nuestros Servicios Especializados de Podología en Alicante</h2>
+        {data.map((item, index) => (
+          <Ribbon key={index} variant={item.variant} imgLeft={item.imgLeft} buttonText={item.buttonText} imgAlt={item.imgAlt} imgSrc={item.imgSrc} title={item.title}>
+            {item.children} 
+          </Ribbon>
+        ))}
+        <h2>Servicios Especializados de Podología en Alicante</h2>
         <h3>biomecánica y estudio de la pisada</h3>
         <p>Nuestra completa exploración biomecánica va más allá de analizar simplemente la marcha y la postura. Realizamos un estudio minucioso del sistema musculo-esquelético, evaluamos la dinámica del gesto deportivo y examinamos la pisada. Esta evaluación detallada nos permite identificar desequilibrios y anomalías, proporcionando una base sólida para tratamientos personalizados que aborden las raíces de las molestias y mejoren la salud de tus pies.</p>
         <button>Más Informacion</button>
--- src/common/ribbon/ribbon.tsx
+++ src/common/ribbon/ribbon.tsx
@@ -5,39 +5,48 @@ import React, { ReactNode } from 'react';
 
 interface RibbonProps {
     title: string;
-    buttonText: string;
-    imgSrc: string;
-    imgAlt: string;
-    variant?: 'primary' | 'secondary';
+    buttonText?: string;
+    imgSrc?: string;
+    imgAlt?: string;
+    variant?: 'primary' | 'secondary' | 'tertiary' | 'quaternary';
     imgLeft?: boolean;
 }
 
 export default function Ribbon({ children, ...props }: RibbonProps & { children: ReactNode }) {
-
+    props.imgSrc = props.imgSrc || '';
+    props.imgAlt = props.imgAlt || '';
+    props.variant = props.variant || 'primary';
     return (
-        <div className={styles.ribbonContainer + ' ' + (props.variant === 'secondary' ? styles.secondary : styles.primary)}>
+        <div className={styles.ribbonContainer + ' ' + styles[props.variant]}>
             {!props.imgLeft &&
                 <div className={styles.firstContainer}>
                     <h3 className={styles.title}>{props.title}</h3>
                     <p className={styles.children}>{children}</p>
-                    <button className={styles.button}> {props.buttonText}</button>
+                    {props.buttonText &&
+                        <button className={styles.button}> {props.buttonText}</button>
+                    }
                 </div>
             }
-            <div className={styles.secondContainer}>
-                <div className={styles.imageContainer}>
-                    <Image
-                        fill={true}
-                        className={styles.image}
-                        src={props.imgSrc}
-                        alt={props.imgAlt} />
+            {props.imgSrc &&
 
+                <div className={styles.secondContainer}>
+                    <div className={styles.imageContainer}>
+                        <Image
+                            fill={true}
+                            className={styles.image}
+                            src={props.imgSrc}
+                            alt={props.imgAlt} />
+
+                    </div>
                 </div>
-            </div>
+            }
             {props.imgLeft &&
                 <div className={styles.firstContainer}>
                     <h3 className={styles.title}>{props.title}</h3>
                     <p className={styles.children}>{children}</p>
-                    <button className={styles.button}> {props.buttonText}</button>
+                    {props.buttonText &&
+                        <button className={styles.button}> {props.buttonText}</button>
+                    }
                 </div>
             }
         </div>
