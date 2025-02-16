785fb90fa4b4e4eac83806eba87c2c41f2229ec1
manu911 <manugc911@gmail.com>
2025-02-16 16:36:55 +0100

    calendar test1

---
+++ src/common/calendar/calendar.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode, SyntheticEvent } from 'react';
+import ApiCalendar from 'react-google-calendar-api';
+
+const config = {
+    "clientId": process.env.CLIENTID_CALENDAR?.toString()?? "",
+    "apiKey": process.env.APIKEY_CALENDAR?.toString() ?? "" ,
+    "scope": "https://www.googleapis.com/auth/calendar",
+    "discoveryDocs": [
+        "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+    ]
+}
+
+const apiCalendar = new ApiCalendar(config)
+
+export default class DoubleButton extends React.Component {
+    constructor(props: object) {
+        super(props);
+        this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    public handleItemClick(event: SyntheticEvent<any>, name: string): void {
+        if (name === 'sign-in') {
+            console.log(apiCalendar.handleAuthClick())
+        } else if (name === 'sign-out') {
+            console.log(apiCalendar.handleSignoutClick());
+        }
+    }
+    public handleCreate(): void {
+        
+        const time:number=25;
+        const summary: string = "my summary";
+        const description: string = "There is no description";
+        const calendarId : string  = "";
+        const timeZone : string = "Europe/Zurich" 
+        apiCalendar.createEventFromNow({time, summary, description}, calendarId , timeZone);
+        
+    }
+
+           
+        
+    
+    render(): ReactNode {
+        return (
+            <>
+                <button
+                    onClick={(e) => this.handleItemClick(e, 'sign-in')}
+                >
+                    sign-in
+                </button>
+                <button
+                    onClick={(e) => this.handleItemClick(e, 'sign-out')}
+                >
+                    sign-out
+                </button>
+                <button
+                    onClick={() => this.handleCreate()}
+                >
+                    create
+                </button>
+            </>
+        );
+    }
+}
+
