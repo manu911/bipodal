7161476489455956fd6fa0ede1daa55afd3a960b
Manu Guevara <manugc911@gmail.com>
2025-02-25 15:28:33 +0100

    reviews 2

--- src/common/ReviewsPage/ReviewsPage.tsx
+++ src/common/ReviewsPage/ReviewsPage.tsx
@@ -3,15 +3,23 @@ import "react-google-reviews/dist/index.css";
 export function ReviewsPage({}) {
   const myHeaders = new Headers();
   const myInit: RequestInit = {
-    method: "GET",
+    method: "POST",
     headers: myHeaders,
     mode: "cors",
     cache: "default",
   };
-  function fetchReviews() {
+function fetchReviews() {
+  //   const myRequest = new Request(
+  //     `https://places.googleapis.com/v1/places/ChIJs5ydyTiuEmsR0fRSlU0C7k0?fields=id,displayName&key=` +
+  //       process.env.GOOGLE_REVIEWS_API_KEY,
+  //     myInit
+  //   );
     const myRequest = new Request(
-      `https://places.googleapis.com/v1/places/ChIJs5ydyTiuEmsR0fRSlU0C7k0?fields=id,displayName&key=` +
-        process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+      `https://maps.googleapis.com/maps/api/place/details/json
+  ?fields=name%2Crating%2Cformatted_phone_number
+  &place_id=ChIJs5ydyTiuEmsR0fRSlU0C7k0
+  &key=` +
+        process.env.GOOGLE_REVIEWS_API_KEY,
       myInit
     );
     const data = fetch(myRequest)
