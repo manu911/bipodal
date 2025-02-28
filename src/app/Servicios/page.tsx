73d34d6465f64ac53fb73e04baed54dba69ee5fb
Manu Guevara <manugc911@gmail.com>
2025-02-28 18:24:50 +0100

    fix build

--- src/app/Servicios/page.tsx
+++ src/app/Servicios/page.tsx
@@ -157,105 +157,105 @@ export default function Servicios() {
       imgSrc: "./Onicomicosis.jpg",
       imgAlt: "",
       description: `En Clínica Bipodal, llevamos la excelencia de nuestros servicios directamente a tu hogar. Entendemos que la comodidad es clave cuando se trata de cuidar de tu salud podológica, por lo que ofrecemos nuestros servicios de podología a domicilio. Ya sea que te encuentres con limitaciones de movilidad, prefieras la privacidad de tu espacio o simplemente busques la máxima conveniencia, nuestro equipo de podólogos expertos está listo para brindarte el mismo nivel de atención especializada que encontrarías en nuestra clínica. `,
-  
+      key: 1,
     },
     {
       title: "Biomecánica y estudio de la pisada",
       imgSrc: "./biomecanica.jpg",
       imgAlt: "pies",
       description: `Nuestra completa exploración biomecánica va más allá de analizar simplemente la marcha y la postura. Realizamos un estudio minucioso del sistema musculo-esquelético, evaluamos la dinámica del gesto deportivo y examinamos la pisada. Esta evaluación detallada nos permite identificar desequilibrios y anomalías, proporcionando una base sólida para tratamientos personalizados que aborden las raíces de las molestias y mejoren la salud de tus pies.`,
-   
+      key: 2,
     },
     {
       title: "Ortesis Plantar",
       imgSrc: "./OrtesisPlantar.jpg",
       imgAlt: "pies",
       description: `Las ortesis plantares que ofrecemos no son solo plantillas; son soluciones a medida para tus pies. Desde la corrección de problemas biomecánicos hasta el alivio de molestias específicas, nuestras ortesis son diseñadas para adaptarse a la anatomía única de tus pies. Al proporcionar soporte personalizado, contribuyen no solo a mejorar la comodidad, sino también a abordar problemas a largo plazo, brindando una solución integral para tus necesidades podológicas.`,
-
+      key: 3,
     },
     {
       title: "Quiropodia",
       imgSrc: "./quiropodia.jpg",
       imgAlt: "pies",
       description: `Nuestro servicio de quiropodia va más allá del cuidado básico de las uñas. Realizamos tratamientos dermatológicos especializados, abordando afecciones de la piel y uñas. Desde el corte y fresado de uñas hasta la eliminación de durezas, cada sesión de quiropodia está diseñada para mantener la salud óptima de tus pies, combinando cuidado estético y terapéutico.`,
-   
+      key: 4,
     },
     {
       title: "Papiloma plantar",
       imgSrc: "./papilomaPlantar.jpg",
       imgAlt: "pies",
       description: `La eliminación de verrugas plantares requiere más que un enfoque convencional. Utilizamos métodos especializados, como el tratamiento con ácido nítrico, asegurando la erradicación completa del papiloma y una curación efectiva de la herida. Nuestra atención no se limita solo a la eliminación; nos centramos en prevenir recurrencias y mantener la integridad de tu piel.`,
-    
+      key: 5,
     },
     {
       title: "Onicomicosis",
       imgSrc: "./Onicomicosis.jpg",
       imgAlt: "pies",
       description: `Integrado dentro de nuestros servicios de quiropodia, el tratamiento de la onicomicosis es abordado con un enfoque integral. No solo aplicamos antimicóticos para tratar la infección, sino que también brindamos orientación sobre cuidados en el hogar y medidas preventivas para garantizar una recuperación duradera y prevenir futuras infecciones.`,
-     
+      key: 6,
     },
     {
       title: "Onicocriptosis o uña encarnada",
       imgSrc: "./Onicocriptosis.jpg",
       imgAlt: "pies",
       description: `Dentro de la quiropodia, ofrecemos tratamiento para la uña encarnada (espiculotomía) siempre que no sea necesario recurrir a la cirugía o anestesia. Nuestra atención se centra en aliviar el dolor y corregir la condición de forma no invasiva, garantizando una recuperación rápida y cómoda.`,
-
+      key: 7,
     },
     {
       title: "Tratamiento y Cuidados de Pie Diabético",
       imgSrc: "./pieDiabetico.jpg",
       imgAlt: "pies",
       description: `Nuestro enfoque en el cuidado de pacientes diabéticos va más allá de la quiropodia estándar. Ofrecemos una quiropodia completa junto con un tratamiento especializado para úlceras y heridas, priorizando la salud y el bienestar de aquellos con condiciones específicas, como la diabetes.`,
-     
+      key: 8,
     },
     {
       title: "Ortoniquia",
       imgSrc: "./ortoniquia.jpg",
       imgAlt: "pies",
       description: `La ortesis ungueal que proporcionamos no solo corrige deformidades, sino que también se centra en la reeducación. Al abordar problemas específicos de las uñas, buscamos mejorar la función y la estética, ofreciendo soluciones personalizadas para mantener la salud de tus pies.`,
-      
+      key: 9,
     },
     {
       title: "Dermatomicosis",
       imgSrc: "./dermatomicosis.jpg",
       imgAlt: "pies",
       description: `Nuestro tratamiento de hongos en la piel va más allá de una solución única. Adaptamos nuestras terapias a la naturaleza específica de la dermatofitosis, aplicando tratamientos personalizados para garantizar resultados efectivos y duraderos.`,
-     
+      key: 10,
     },
     {
       title: "Podología física",
       imgSrc: "./podologiaFisica.jpg",
       imgAlt: "pies",
       description: `Nuestro servicio de podología física va más allá de la aplicación de vendajes y técnicas en lesiones articulares y ligamentosas. Cada sesión se adapta a tus necesidades específicas, utilizando técnicas avanzadas para mejorar la función y reducir el dolor.`,
-      
+      key: 11,
     },
     {
       title: "Podología infantil",
       imgSrc: "./podologiaInfantil.jpg",
       imgAlt: "pies",
       description: `En nuestra clínica, nos preocupamos en atender las necesidades únicas de los niños y sus pies en crecimiento. Nuestro equipo de podólogos con amplia experiencia está aquí para proporcionar tratamientos suaves y efectivos, desde el cuidado básico hasta la solución de problemas específicos como deformidades o dolor.`,
-    
+      key: 12,
     },
     {
       title: "Cirugía Ungueal",
       imgSrc: "./cirugiaUngueal.jpg",
       imgAlt: "pies",
       description: `La cirugía ungueal que realizamos aborda onicocriptosis y otras afecciones mediante cirugía menor. Adaptamos el procedimiento según el canal ungueal afectado, asegurando una solución duradera y una recuperación rápida.`,
-    
+      key: 13,
     },
     {
       title: "Infiltraciones podológicas",
       imgSrc: "./infiltracionesPodologicas.jpg",
       imgAlt: "pies",
       description: `Ofrecemos el servicio de infiltraciones en Clínica Bipodal, una solución eficaz para disminuir la inflamación directamente en el punto de dolor. Nuestros podólogos especializados realizan infiltraciones de corticoides con precisión, brindando alivio a los pacientes que experimentan molestias persistentes. Descubre cómo este tratamiento puede ayudarte a recuperar la movilidad y disfrutar de una vida sin dolor.`,
-      
+      key: 14,
     },
     {
       title: "Neuroma de Morton",
       imgSrc: "./cirugiaUngueal.jpg",
       imgAlt: "pies",
       description: `En Clínica Bipodal, abordamos el Neuroma de Morton con enfoques especializados para aliviar las molestias y mejorar la calidad de vida. Nuestros podólogos expertos realizan evaluaciones detalladas para comprender la naturaleza de este problema neurológico en los pies. Descubre cómo nuestro tratamiento personalizado puede proporcionar alivio y restaurar la comodidad en tus pasos.`,
-   
+      key: 15,
     },
   ];
 
--- src/common/acordeon/acordeon.tsx
+++ src/common/acordeon/acordeon.tsx
@@ -2,22 +2,27 @@
 import styles from "./acordeon.module.css";
 import React from "react";
 import Image from "next/image";
-export interface AcordeonProps {
+export interface AcordeonItemProps {
   title: string;
   description: string;
   imgSrc: string;
   imgAlt: string;
+  key: number;
 }
 
-export default function Acordeon(props: AcordeonProps[]) {
+export interface AcordeonProps {
+  data: AcordeonItemProps[];
+}
+
+export default function Acordeon(props: AcordeonProps) {
   return (
     <div className={styles.acordeonContainer}>
       {props.data.map((el) => {
         return (
-          <div className={styles.imageSlot}>
+          <div className={styles.imageSlot} key={el.key}>
             <Image className={styles.image} src={el.imgSrc} fill/>
-            <span>{el.title}</span>
-            <p>{el.description}</p>
+            <span className={styles.title}>{el.title}</span>
+            <p className={styles.description}>{el.description}</p>
           </div>
         );
       })}
